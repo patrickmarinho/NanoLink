bbaf6defe776f76b26fb2c9e1edd596c91253af4
Erick Patrick <erick.marinho2003@gmail.com>
2025-05-09 01:31:56 -0300

    first commit

---
+++ src/test/java/com/nanolink/nanolink_service/NanolinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanolink.nanolink_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanolinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
